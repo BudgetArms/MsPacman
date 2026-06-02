f4ed93a7cf270f99eb9c9cb00dcdd79bf7121195
BudgetArms <67414205+BudgetArms@users.noreply.github.com>
2026-06-02 20:30:05 +0200

    Added LevelManagerComponent
    This is still a work in progress

---
+++ Game/Components/LevelManagerComponent.cpp
@@ -0,0 +1,303 @@
+#include "LevelManagerComponent.hpp"
+
+#include <fstream>
+
+#include <nlohmann/json.hpp>
+
+#include "LevelGridComponent.hpp"
+#include "RenderCenterComponent.hpp"
+#include "Base/Events.hpp"
+#include "Core/HelperFunctions.hpp"
+#include "Core/Scene.hpp"
+#include "Managers/ResourceManager.hpp"
+#include "Managers/SceneManager.hpp"
+
+
+using namespace Game;
+
+
+LevelManagerComponent::LevelManagerComponent(bae::GameObject& owner, const GameMode gameMode) :
+    Component(owner),
+    m_GameMode{ gameMode }
+{
+    m_BackgroundSpriteSheet = std::make_unique<bae::SpriteSheet>(m_BackgroundTexturePath,
+                                                                 SDL_FRect(0, 0, 224, 1488), 1, 6);
+    bae::SceneManager::GetInstance().CreateScene(m_LevelSceneName);
+}
+
+LevelManagerComponent::~LevelManagerComponent()
+{
+    ClearLevel();
+}
+
+void LevelManagerComponent::Render() const
+{
+    m_BackgroundSpriteSheet->Render();
+    bae::Utils::FillCircle(m_BackgroundSpriteSheet->m_Position, 10, bae::Utils::Color::Green);
+}
+
+void LevelManagerComponent::LoadLevelFromFile(int levelNumber, const std::filesystem::path& jsonFile)
+{
+    if(levelNumber < 0)
+    {
+        std::cout << FUNCTION_NAME << " Failed To Set LevelNumber! LevelNumber Cannot Be Negative!" << '\n';
+        return;
+    }
+
+    const auto resourceFolder = bae::ResourceManager::GetInstance().GetResourcesPath();
+    if(!std::filesystem::exists(resourceFolder / jsonFile))
+    {
+        throw std::runtime_error(
+            FUNCTION_NAME + std::string(" Failed! File Not found! file") + jsonFile.string());
+    }
+
+    std::ifstream file{ resourceFolder / jsonFile, std::ios::binary };
+    if(!file.is_open())
+    {
+        throw std::runtime_error(FUNCTION_NAME + std::string(" Failed To Open File: ") + jsonFile.string());
+    }
+
+    nlohmann::json json{};
+    LevelJson levelJson{};
+
+    try
+    {
+        file >> json;
+        levelJson = json.get<LevelJson>();
+    }
+    catch(const std::exception& exception)
+    {
+        throw std::runtime_error(FUNCTION_NAME + std::string(" Failed To Parse Json File! Exception: ")
+            + std::string(exception.what()));
+    }
+
+    m_LevelJson.insert({ levelNumber, levelJson });
+}
+
+void LevelManagerComponent::CreateLevel()
+{
+    const std::optional<LevelJson> currentLevelJson = GetCurrentLevelJson();
+    if(!currentLevelJson.has_value())
+    {
+        return;
+    }
+
+    LevelJson levelJson = currentLevelJson.value();
+
+    // TODO: add implementation
+    CreateBackground();
+    CreateGrid();
+}
+
+void LevelManagerComponent::ResetLevel()
+{
+    ClearLevel();
+    CreateLevel();
+}
+
+
+int LevelManagerComponent::GetCurrentLevel() const
+{
+    return m_CurrentLevel;
+}
+
+void LevelManagerComponent::Notify(const unsigned int eventHash, bae::Subject*)
+{
+    switch(GetEvent(eventHash))
+    {
+        // case Events::GamePaused:
+        // break;
+        // case Events::GameResumed:
+        // break;
+        case Events::PlayerDied:
+            break;
+        case Events::GameOver:
+            ResetLevel();
+            break;
+        case Events::LevelWon:
+            WonLevel();
+            break;
+        case Events::LevelLost:
+            LostLevel();
+            break;
+        case Events::GhostDied:
+            break;
+        case Events::BeginLevel:
+            break;
+        case Events::RestartLevel:
+            break;
+        case Events::ScoreChanged:
+            break;
+        case Events::LivesChanged:
+            break;
+        case Events::InvincibilityChanged:
+            break;
+        case Events::NoEvent:
+            break;
+    }
+}
+
+void LevelManagerComponent::SetSpriteSheetWorldLocation(const glm::vec2& location) const
+{
+    m_BackgroundSpriteSheet->m_Position = location;
+}
+
+void LevelManagerComponent::SetSpriteSheetWorldRotation(const float rotation) const
+{
+    m_BackgroundSpriteSheet->m_Rotation = rotation;
+}
+
+
+void LevelManagerComponent::SetSpriteSheetWorldScale(const glm::vec2& scale) const
+{
+    m_BackgroundSpriteSheet->m_Scale = scale;
+}
+
+
+void LevelManagerComponent::WonLevel()
+{
+    ++m_CurrentLevel;
+    ClearLevel();
+    CreateLevel();
+}
+
+void LevelManagerComponent::LostLevel()
+{
+    ResetLevel();
+}
+
+void LevelManagerComponent::LoadBackground(const int levelIndex) const
+{
+    if(m_LevelJson.contains(levelIndex))
+    {
+        throw std::runtime_error(
+            FUNCTION_NAME + std::string(" Failed! LevelJson Not Found! Index: " + std::to_string(levelIndex)));
+    }
+
+    m_BackgroundSpriteSheet->m_Index = levelIndex;
+}
+
+void LevelManagerComponent::ClearLevel() const
+{
+    // To avoid a deletion of objects if the scene is already getting deleted
+    if(m_Owner->IsMarkedForDeletion())
+    {
+        return;
+    }
+
+    const bae::Scene* const scene = bae::SceneManager::GetInstance().GetScene(m_LevelSceneName);
+    if(scene)
+    {
+        scene->RemoveAll();
+    }
+}
+
+std::optional<LevelJson> LevelManagerComponent::GetCurrentLevelJson()
+{
+    if(m_LevelJson.empty())
+    {
+        return std::nullopt;
+    }
+
+    const int currentLevelIndex = static_cast<int>(static_cast<float>(m_CurrentLevel)
+        / m_LevelRepeatTimes) % m_LevelJson.size();
+
+    const auto levelJsonIt = m_LevelJson.find(currentLevelIndex);
+    if(levelJsonIt == m_LevelJson.end())
+    {
+        return std::nullopt;
+    }
+
+    return levelJsonIt->second;
+}
+
+void LevelManagerComponent::CreateBackground()
+{
+}
+
+void LevelManagerComponent::CreateGrid()
+{
+    const std::optional<LevelJson> currentLevelJson = GetCurrentLevelJson();
+    if(!currentLevelJson.has_value())
+    {
+        return;
+    }
+
+
+    LevelJson levelJson = currentLevelJson.value();
+
+    const bae::WindowSize gridDimensions = levelJson.Dimensions;
+    const glm::vec2 gridSize             = { gridDimensions.Width, gridDimensions.Height };
+    const int nrColumns                  = levelJson.NrColumns;
+    const int nrRows                     = levelJson.NrRows;
+
+    bae::Scene* scene = bae::SceneManager::GetInstance().GetScene(m_LevelSceneName);
+    if(!scene)
+    {
+        throw std::runtime_error(FUNCTION_NAME + std::string(" Failed To Get LevelScene!"));
+    }
+
+    auto levelGrid                   = std::make_shared<bae::GameObject>("LevelGrid");
+    const bae::WindowSize windowSize = bae::Renderer::GetInstance().GetSDLWindowSize();
+
+    // gridSize
+    levelGrid->SetWorldLocation({ -gridSize.x / 2.f, -gridSize.y / 2.f });
+    levelGrid->AddLocation({ windowSize.Width / 2.f, windowSize.Height / 2.f });
+
+    levelGrid->AddComponent<LevelGridComponent>(*levelGrid, gridSize, nrColumns, nrRows);
+    [[maybe_unused]] auto const levelGridComponent = levelGrid->GetComponent<Game::LevelGridComponent>();
+
+    levelGridComponent->RemoveNode(bae::Graphs::GridPosition{ 0, 0 });
+    levelGridComponent->RemoveNode(bae::Graphs::GridPosition{ 0, 1 });
+    levelGridComponent->RemoveNode(bae::Graphs::GridPosition{ 0, 2 });
+
+    levelGridComponent->RemoveNode(bae::Graphs::GridPosition{ 0, 0 });
+    levelGridComponent->RemoveNode(bae::Graphs::GridPosition{ 1, 0 });
+    levelGridComponent->RemoveNode(bae::Graphs::GridPosition{ 2, 0 });
+
+    for(const bae::Graphs::GridPosition& gridPosition : levelJson.NodesToRemove)
+    {
+        levelGridComponent->RemoveNode(gridPosition);
+    }
+
+    for(const bae::Graphs::GridPosition& gridPosition : levelJson.NodesToAddAfterRemoval)
+    {
+        levelGridComponent->AddNode(gridPosition);
+        levelGridComponent->AddConnectionsToNeighbors(gridPosition);
+    }
+
+
+    // levelGridComponent->SetRenderCells(true);
+    // levelGridComponent->SetRenderNodes(true);
+    levelGridComponent->SetRenderConnections(true);
+
+    scene->Add(levelGrid);
+}
+
+void LevelManagerComponent::HandleEvent(const unsigned int eventHash)
+{
+    switch(GetEvent(eventHash))
+    {
+        case Events::PlayerDied:
+        case Events::GameOver:
+        case Events::LevelWon:
+            break;
+        case Events::LevelLost:
+            break;
+        case Events::GhostDied:
+            break;
+        case Events::BeginLevel:
+            CreateLevel();
+            break;
+        case Events::RestartLevel:
+            break;
+        case Events::ScoreChanged:
+        case Events::LivesChanged:
+        case Events::InvincibilityChanged:
+        case Events::NoEvent:
+            break;
+    }
+}
+
+
+
---
+++ Game/Components/LevelManagerComponent.hpp
@@ -0,0 +1,71 @@
+#pragma once
+#include <filesystem>
+#include <string>
+
+#include <nlohmann/json.hpp>
+
+#include "Components/Component.hpp"
+#include "Core/Observer.hpp"
+#include "Core/SpriteSheet.hpp"
+
+#include "Base/GameMode.hpp"
+#include "Base/Level.hpp"
+#include "Core/EventListener.hpp"
+
+
+namespace Game
+{
+    class LevelManagerComponent : public bae::Component, public bae::Observer, public bae::EventListener
+    {
+    public:
+        LevelManagerComponent(bae::GameObject& owner, GameMode gameMode);
+        ~LevelManagerComponent() override;
+
+        void Render() const override;
+
+        void LoadLevelFromFile(int levelNumber, const std::filesystem::path& jsonFile);
+
+        void CreateLevel();
+        void ResetLevel();
+
+
+        [[nodiscard]] int GetCurrentLevel() const;
+
+        void Notify(unsigned int eventHash, bae::Subject* subject) override;
+
+        void SetSpriteSheetWorldLocation(const glm::vec2& location) const;
+        void SetSpriteSheetWorldRotation(float rotation) const;
+        void SetSpriteSheetWorldScale(const glm::vec2& scale) const;
+
+    private:
+        void WonLevel();
+        void LostLevel();
+
+        void LoadBackground(int levelIndex) const;
+
+        void ClearLevel() const;
+
+        [[nodiscard]] std::optional<LevelJson> GetCurrentLevelJson();
+
+        void CreateBackground();
+        void CreateGrid();
+
+
+        void HandleEvent(unsigned int eventHash) override;
+
+        std::unique_ptr<bae::SpriteSheet> m_BackgroundSpriteSheet{};
+
+        GameMode m_GameMode;
+
+
+        int m_CurrentLevel{};
+
+
+        std::unordered_map<int, LevelJson> m_LevelJson{};
+
+        const std::filesystem::path m_BackgroundTexturePath{ "Textures/Level/Levels.png" };
+        const std::string m_LevelSceneName{ "LevelScene" };
+        static constexpr int m_LevelRepeatTimes{ 2 };
+        static constexpr int m_LevelTextures{ 4 };
+    };
+}
