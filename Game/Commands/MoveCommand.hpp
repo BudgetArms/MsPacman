dccfac40e9d7bfd05cfb243d225321be6234f20a
BudgetArms <67414205+BudgetArms@users.noreply.github.com>
2026-06-03 13:34:19 +0200

    Removed EventListener from MoveCommand

--- Game/Commands/MoveCommand.hpp
+++ Game/Commands/MoveCommand.hpp
@@ -3,48 +3,17 @@
 #include "Commands/GameActorCommand.hpp"
 
 #include "Base/DirectionEnum.hpp"
-#include "Base/Events.hpp"
-#include "Core/EventListener.hpp"
-#include "Core/HelperFunctions.hpp"
 
 
 namespace Game
 {
-    class MoveCommand final : public bae::GameActorCommand, public bae::EventListener
+    class MoveCommand final : public bae::GameActorCommand
     {
     public:
         MoveCommand(bae::GameObject& owner, Direction direction, float speed);
+        ~MoveCommand() = default;
 
         void Execute() override;
-
-        void HandleEvent(const unsigned int eventHash) override
-        {
-            const Events event = GetEvent(eventHash);
-
-            std::cout << "MoveCommand::HandleEvent: ";
-            switch(event)
-            {
-                // case Events::GamePaused:
-                // case Events::GameResumed:
-                case Events::PlayerDied:
-                    break;
-                case Events::GameOver:
-                    std::cout << " GameOver" << '\n';
-                    break;
-                case Events::GhostDied:
-                    break;
-                case Events::BeginLevel:
-                    std::cout << " GameOver" << '\n';
-                    break;
-                case Events::RestartLevel:
-                case Events::ScoreChanged:
-                case Events::NoEvent:
-                case Events::LivesChanged:
-                case Events::InvincibilityChanged:
-                default:
-                    break;
-            }
-        }
 
     private:
         Direction m_Direction;
