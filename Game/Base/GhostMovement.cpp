fa403cc8dc09c947e1f4d5aa4969986863d5a248
BudgetArms <67414205+BudgetArms@users.noreply.github.com>
2026-06-11 17:11:52 +0200

    Added GhostMovement
    Not fully implemented yet

---
+++ Game/Base/GhostMovement.cpp
@@ -0,0 +1,115 @@
+#include "GhostMovement.hpp"
+
+#include "Singletons/GameTime.hpp"
+
+#include "Components/GridMovementComponent.hpp"
+#include "Components/LevelGridComponent.hpp"
+
+
+using namespace Game;
+
+
+GhostMovement::GhostMovement(bae::GameObject& ghostObject, GridMovementComponent& gridMovementComponent, float speed) :
+    m_GhostObject{ &ghostObject },
+    m_GridMovementComponent{ &gridMovementComponent },
+    m_Speed{ speed }
+{
+}
+
+GhostMovement::~GhostMovement() = default;
+
+void GhostMovement::Update()
+{
+    UpdatePath();
+    SetDirectionFromPath();
+}
+
+void GhostMovement::SetTargetPosition(const glm::vec2& targetPosition)
+{
+    if(m_TargetPosition == targetPosition)
+    {
+        m_bDirtyTargetPosition = false;
+        return;
+    }
+
+    m_bDirtyTargetPosition    = true;
+    m_RequestedTargetPosition = targetPosition;
+}
+
+
+void GhostMovement::UpdatePath()
+{
+    m_ElapsedTime += bae::GameTime::GetInstance().GetDeltaTime();
+    if(m_ElapsedTime < m_RecalculatePathCooldownTime)
+    {
+        return;
+    }
+
+    m_ElapsedTime = 0.0f;
+
+    const glm::vec2 currentPos = m_GhostObject->GetWorldLocation();
+    const auto levelGridComp   = m_GridMovementComponent->GetLevelGridComponent();
+    m_PathToTarget             = levelGridComp->GetShortestPath(currentPos, m_TargetPosition);
+    if(m_PathToTarget.empty())
+    {
+        return;
+    }
+
+
+    if(m_PathToTarget.empty())
+    {
+        return;
+    }
+
+    if(glm::distance(currentPos, m_PathToTarget.front()) < m_MinDistanceToCell)
+    {
+        m_PathToTarget.erase(m_PathToTarget.begin());
+    }
+}
+
+void GhostMovement::SetDirectionFromPath()
+{
+    if(m_PathToTarget.empty())
+    {
+        return;
+    }
+
+    const glm::vec2 currentPos   = m_GhostObject->GetWorldLocation();
+    const glm::vec2 pathPosition = m_PathToTarget.front();
+
+    const glm::vec2 directionToPathPos = pathPosition - currentPos;
+
+    Direction direction{};
+    if(std::abs(directionToPathPos.x) > std::abs(directionToPathPos.y))
+    {
+        // Left or Right
+        if(directionToPathPos.x < 0)
+        {
+            direction = Direction::Left;
+        }
+        else
+        {
+            direction = Direction::Right;
+        }
+    }
+    else
+    {
+        // Down or Up
+        if(directionToPathPos.y < 0)
+        {
+            direction = Direction::Up;
+        }
+        else
+        {
+            direction = Direction::Down;
+        }
+    }
+
+    m_GridMovementComponent->SetDirection(direction);
+
+    if(glm::distance(currentPos, pathPosition) < m_MinDistanceToCell)
+    {
+        m_PathToTarget.erase(m_PathToTarget.begin());
+    }
+}
+
---
+++ Game/Base/GhostMovement.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+
+
+namespace bae
+{
+    class GameObject;
+}
+
+namespace Game
+{
+    class GridMovementComponent;
+
+    class GhostMovement
+    {
+    public:
+        explicit GhostMovement(bae::GameObject& ghostObject, GridMovementComponent& gridMovementComponent, float speed);
+        virtual ~GhostMovement();
+
+        void Update();
+
+        void SetTargetPosition(const glm::vec2& targetPosition);
+
+
+        float m_Speed;
+
+    private:
+        void UpdatePath();
+        void SetDirectionFromPath();
+
+
+        bae::GameObject* m_GhostObject{};
+        GridMovementComponent* m_GridMovementComponent{};
+
+        glm::vec2 m_TargetPosition{};
+        glm::vec2 m_RequestedTargetPosition{};
+
+        std::vector<glm::vec2> m_PathToTarget{};
+
+        const float m_MinDistanceToCell{ 5.f };
+        const float m_RecalculatePathCooldownTime{ 0.5f };
+        float m_ElapsedTime{};
+
+        bool m_bDirtyTargetPosition{};
+    };
+}
